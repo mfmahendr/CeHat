6aa3a57528288adc30338482a6b19ea21cf931fe
M. Fadhil Mahendra <71614957+mfmahendr@users.noreply.github.com>
2021-04-29 22:42:43 +0700

    Update obat.cs

--- cehat/obat.cs
+++ cehat/obat.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace cehat
 {
-    class obat : informasi
+    public class Obat : Informasi
     {
+        private string dosis;
+        private string efekSamping;
+
+
+
     }
 }

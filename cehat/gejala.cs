35accb5c81e1233bb25dab7fa8e538223f704263
M. Fadhil Mahendra <71614957+mfmahendr@users.noreply.github.com>
2021-04-29 22:41:47 +0700

    Update gejala.cs

--- cehat/gejala.cs
+++ cehat/gejala.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,39 @@ using System.Threading.Tasks;
 
 namespace cehat
 {
-    class gejala
+    public class Gejala
     {
+        private int id;
+        private List<string> gejala;
+        private bool status;
+        private bool jawaban;
 
+        public void Soal(string gejala)
+        {
+            Console.WriteLine($"Apakah anda mengalami {gejala} ?");
+        }
+        
+        public void Terjawab(bool jawaban, bool status)
+        {
+            if(jawaban = true)
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+        }
+
+        public bool IsExist(string gejalaDicek)
+        {
+            bool isExist = false;
+            foreach(string i in this.gejala)
+            {
+                if (gejalaDicek == i) { isExist = true; }
+            }
+
+            return isExist;
+        }
     }
 }

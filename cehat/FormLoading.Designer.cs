6ac56f18345070def49369b7be81e84f7f49dbc6
Iqbal M <muhiqbalmasykuri@gmail.com>
2021-04-30 08:10:02 +0700

    make masuk button bigger

--- cehat/FormLoading.Designer.cs
+++ cehat/FormLoading.Designer.cs
@@ -64,9 +64,9 @@ namespace cehat
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(588, 299);
+            this.pictureBox1.Location = new System.Drawing.Point(575, 293);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(104, 45);
+            this.pictureBox1.Size = new System.Drawing.Size(123, 53);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;

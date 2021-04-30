8259e428b87f3f863a75bfb7c595d4ffd114389c
M. Fadhil Mahendra <71614957+mfmahendr@users.noreply.github.com>
2021-04-30 09:50:57 +0700

    Update gejala.cs
    Kagak ada error sih tapi masih abstrak bener :-)

--- cehat/gejala.cs
+++ cehat/gejala.cs
@@ -13,11 +13,14 @@ namespace cehat
         private bool status;
         private bool jawaban;
 
-        public void Soal(string gejala)
+        public int Id { get => id; set => id = value; }
+        public List<string> GejalaPenyakit { get => gejala; set => gejala = value; }
+
+        public Gejala(List<string> gejala)
         {
-            Console.WriteLine($"Apakah anda mengalami {gejala} ?");
+            this.gejala = gejala;
         }
-        
+             
         public void Terjawab(bool jawaban, bool status)
         {
             if(jawaban = true)
@@ -39,6 +42,29 @@ namespace cehat
             }
 
             return isExist;
+        }
+
+        public void Tambah(List<string> gejala)
+        {
+            List<string> gejalaTambahan = new List<string>();
+            foreach(string i in gejala)
+            {
+                if (!IsExist(i))
+                {
+                    gejalaTambahan.Add(i);
+                    Console.WriteLine("Gejala masukan belum ada sehingga informasi penyakit baru bisa ditambahkan. Tapi cari tahu dulu penyakit apa yang dideritanya");
+                                    }
+                else { Console.WriteLine("Gejala sudah ada"); }
+            }
+
+            string namaPenyakitTambahan = Console.ReadLine();
+            Console.WriteLine("Apa nama obatnya?");
+            string namaObat = Console.ReadLine();
+            Console.WriteLine("Dosisnya bagaimana?");
+            string dosisObat = Console.ReadLine();
+            Console.WriteLine("Apa efek samping obat?");
+            string efekSampingObat = Console.ReadLine();
+            Penyakit tambahanPenyakit = new Penyakit(namaPenyakitTambahan, new Gejala(gejalaTambahan), new Obat(namaObat, dosisObat, efekSampingObat));
         }
     }
 }
